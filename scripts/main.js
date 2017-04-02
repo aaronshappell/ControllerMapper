d3db20004624f49c1202efd51623cd7112de2734
aaronshappell <crumster88@gmail.com>
2017-04-02 23:25:40 +0300

    Add basic button selection

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,31 @@
 function updateButton(){
-    var name = document.getElementById("currentButtonName").innerHTML;
-    var action = document.getElementById("currentButtonAction").value;
-    document.getElementById(name).setAttribute("onclick", "selectButton({name:this.id, action:'" + action + "'})");
+    //var name = document.getElementById("currentButtonName").innerHTML;
+    //var action = document.getElementById("currentButtonAction").value;
+    //document.getElementById(name).setAttribute("onclick", "selectButton({name:this.id, action:'" + action + "'})");
 }
 
 function selectButton(data){
-    document.getElementById("currentButtonName").innerHTML = data.name;
-    document.getElementById("currentButtonAction").value = data.action;
+    var table = document.getElementById("selectedButton");
+    while(table.hasChildNodes()){
+        table.removeChild(table.firstChild);
+    }
+
+    for(i = 0; i < data.length; i += 2){
+        var row = document.createElement("tr");
+
+        var nameCell = document.createElement("td");
+        nameCell.appendChild(document.createTextNode(data[i]));
+        row.appendChild(nameCell);
+
+        var actionCell = document.createElement("td");
+        var input = document.createElement("input");
+        input.setAttribute("type", "text");
+        input.setAttribute("value", data[i + 1]);
+        input.setAttribute("onkeyup", "updateButton()");
+        actionCell.appendChild(input);
+        actionCell.appendChild(document.createElement("br"));
+        row.appendChild(actionCell);
+
+        table.appendChild(row);
+    }
 }
