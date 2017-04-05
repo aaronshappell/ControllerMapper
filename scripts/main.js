3439de9bc8c57a6668a7c2804fef1f8f10e74811
aaronshappell <crumster88@gmail.com>
2017-04-05 16:29:55 +0300

    Remove inline onclicks

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,112 @@
+var idList = {
+    "a": {
+        "labelNames": ["A"]
+    },
+    "b": {
+        "labelNames": ["B"]
+    },
+    "x": {
+        "labelNames": ["X"]
+    },
+    "y": {
+        "labelNames": ["Y"]
+    },
+    "left": {
+        "labelNames": ["Left Trigger", "Left Bump"]
+    },
+    "right": {
+        "labelNames": ["Right Trigger", "Right Bump"]
+    },
+    "leftjoy": {
+        "labelNames": ["Left Joystick Button", "Left Joystick X", "Left Joystick Y"]
+    },
+    "rightjoy": {
+        "labelNames": ["Right Joystick Button", "Right Joystick X", "Right Joystick Y"]
+    },
+    "dpad": {
+        "labelNames": ["DPAD Up", "DPAD Down", "DPAD Left", "DPAD Right"]
+    },
+    "back": {
+        "labelNames": ["Back"]
+    },
+    "start": {
+        "labelNames": ["Start"]
+    }
+};
+
+$(".highlight").hide();
+$(document).ready(function() {
+    for(var id in idList){
+        let element = $("#" + id);
+        let highlight = $("#h" + id);
+        let idData = idList[id];
+        element.on("click", function(){
+            $(".highlight").hide();
+            highlight.fadeIn();
+            for(var name in idData.labelNames){
+                //create table row
+                console.log(idData.labelNames[name]);
+            }
+        });
+    }
+});
+
+/*
+var MyApp = function(){
+    var idList = {
+        "a": {
+            "labelNames": ["A"]
+        },
+        "b": {
+            "labelNames": ["B"]
+        },
+        "x": {
+            "labelNames": ["X"]
+        },
+        "y": {
+            "labelNames": ["Y"]
+        },
+        "left": {
+            "labelNames": ["Left Trigger", "Left Bump"]
+        },
+        "right": {
+            "labelNames": ["Right Trigger", "Right Bump"]
+        },
+        "leftjoy": {
+            "labelNames": ["Left Joystick Button", "Left Joystick X", "Left Joystick Y"]
+        },
+        "rightjoy": {
+            "labelNames": ["Right Joystick Button", "Right Joystick X", "Right Joystick Y"]
+        },
+        "dpad": {
+            "labelNames": ["DPAD Up", "DPAD Down", "DPAD Left", "DPAD Right"]
+        },
+        "back": {
+            "labelNames": ["Back"]
+        },
+        "start": {
+            "labelNames": ["Start"]
+        }
+    };
+};
+MyApp.prototype.init = function () {
+    for(var id in this.idList){
+        let element = $(id);
+        let idData = this.idList[id];
+        element.on("click", function(){
+            for(var name in idData.labelNames){
+                //create table row
+                console.log(name);
+            }
+        });
+    }
+}
+var app = new MyApp();
+$(".highlight").hide();
+$(document).on("load", app.init());
+*/
+
+/*
 $(".highlight").hide(); //Don't wait for document to load
 
 function updateButton(){
@@ -10,7 +119,7 @@ function selectButton(id, data){
     $(".highlight").hide();
     $("#h" + id).fadeIn();
 
-    $("#inputcontainer").hide();
+    $("#inputContainer").hide();
     var table = document.getElementById("selectedButton");
     while(table.hasChildNodes()){
         table.removeChild(table.firstChild);
@@ -34,5 +143,6 @@ function selectButton(id, data){
 
         table.appendChild(row);
     }
-    $("#inputcontainer").fadeIn();
+    $("#inputContainer").fadeIn();
 }
+*/
